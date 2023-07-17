cdcca26bcb2f19fe27cf0a7845dc566144ec6a6a
wiiznokes <78230769+wiiznokes@users.noreply.github.com>
2023-07-17 13:06:42 +0200

    hello

---
+++ src/main.rs
@@ -0,0 +1,43 @@
+use iced::{Application, executor, Command, widget::Text};
+
+fn main() {
+    println!("Hello, world!");
+}
+
+
+
+
+struct App {
+
+}
+
+#[derive(Debug)]
+enum AppMsg {
+    
+}
+
+
+impl Application for App {
+    type Executor = executor::Default;
+    type Message = AppMsg;
+    type Theme = iced::Theme;
+    type Flags = ();
+
+    fn new(_flags: Self::Flags) -> (Self, iced::Command<Self::Message>) {
+        (App {}, Command::none())
+    }
+
+    fn title(&self) -> String {
+        String::from("App")
+    }
+
+    fn update(&mut self, message: Self::Message) -> iced::Command<Self::Message> {
+       
+        Command::none()
+    }
+
+    fn view(&self) -> iced::Element<'_, Self::Message, iced::Renderer<Self::Theme>> {
+        Text::new("hello")
+        .into()
+    }
+}
